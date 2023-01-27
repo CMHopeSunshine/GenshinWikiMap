0635a957c5eeef1287b8cdedfc193ea3a5d911f1
CMHopeSunshine <277073121@qq.com>
2023-01-27 22:32:54 +0800

    :sparkles: 更新zip内容

--- generate.py
+++ generate.py
@@ -2,9 +2,9 @@ import zipfile
 import hashlib
 from pathlib import Path
 
-from path import DATA
 from utils import save_json
 
+DATA = Path() / 'data'
 single_file = ['元素精通系数', '原魔列表', '圣遗物信息', '圣遗物列表', '有效词条', '武器列表', '武器类型', '类型', '角色列表', '属性Map']
 
 json_data = {
@@ -12,51 +12,36 @@ json_data = {
     'weapon':   [],
     'artifact': []
 }
-zip_file: dict[str, zipfile.ZipFile] = {
-    'avatar': zipfile.ZipFile(DATA / 'avatar' / 'avatar.zip', 'w', zipfile.ZIP_STORED),
-    'weapon': zipfile.ZipFile(DATA / 'weapon' / 'weapon.zip', 'w', zipfile.ZIP_STORED),
-    'artifact': zipfile.ZipFile(DATA / 'artifact' / 'artifact.zip', 'w', zipfile.ZIP_STORED)
-}
 
-for type in json_data:
+for type, value in json_data.items():
     for file in (DATA / type).iterdir():
-        if file.suffix != '.json':
-            continue
-        zip_file[type].write(file, file.name)
-        json_data[type].append(
-            {
-                'name': file.stem,
-                'hash': hashlib.md5(file.read_bytes()).hexdigest()
-            }
-        )
-for zip_ in zip_file.values():
-    zip_.close()
+        if file.suffix == '.json':
+            value.append(
+                {
+                    'name': file.stem,
+                    'hash': hashlib.md5(file.read_bytes()).hexdigest(),
+                }
+            )
 
-json_data['data'] = []
-data_zip = zipfile.ZipFile(DATA / 'data.zip', 'w', zipfile.ZIP_STORED)
-for file in single_file:
-    path = DATA / f'{file}.json'
-    data_zip.write(path, path.name)
-    json_data['data'].append(
-        {
-            'name': path.stem,
-            'hash': hashlib.md5(path.read_bytes()).hexdigest()
-        }
-    )
-
-data_zip.close()
+json_data['data'] = [{
+    'name': (path := DATA / f'{file}.json').stem,
+    'hash': hashlib.md5(path.read_bytes()).hexdigest()
+} for file in single_file]
 
 save_json(json_data, DATA / 'data_list.json')
 
+with zipfile.ZipFile(DATA / 'data.zip', 'w', zipfile.ZIP_STORED) as f:
+    for file in DATA.rglob('*'):
+        if file.suffix == '.zip':
+            continue
+        if file.name == 'raw' or file.parent.name == 'raw' or file.parent.parent.name == 'raw':
+            continue
+        if file.is_file() and file.stem not in single_file and file.parent.name == 'data':
+            continue
+        f.write(file, str(file).replace('\\', '/').lstrip('data').lstrip('/'))
 
-resources_zip = zipfile.ZipFile(Path() / 'resources' / 'genshin_resources.zip', 'w', zipfile.ZIP_DEFLATED)
 
-for dir_type in ['talent', 'splash', 'material', 'avatar', 'avatar_side', 'weapon', 'artifact']:
-    for file in (Path() / 'resources' / dir_type).iterdir():
-        resources_zip.write(file, str(file).replace('\\', '/').lstrip('resources').lstrip('/'))
-
-
-resources_zip.close()
-
-
-
+with zipfile.ZipFile(Path() / 'resources' / 'genshin_resources.zip', 'w', zipfile.ZIP_DEFLATED) as f:
+    for dir_type in ['talent', 'splash', 'material', 'avatar', 'avatar_side', 'weapon', 'artifact']:
+        for file in (Path() / 'resources' / dir_type).iterdir():
+            f.write(file, str(file).replace('\\', '/').lstrip('resources').lstrip('/'))
